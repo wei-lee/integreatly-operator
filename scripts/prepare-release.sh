a7c31d640f133d89c53a7b671e277e126955c416
Wei Li <wei.li@redhat.com>
2020-04-22 16:34:08 +0100

    Update prepare-release.sh

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -5,7 +5,7 @@ set -o pipefail
 PREVIOUS_VERSION="$(cat deploy/olm-catalog/integreatly-operator/integreatly-operator.package.yaml | grep integreatly-operator | awk -F v '{print $2}')"
 
 create_new_csv() {
-  operator-sdk generate csv --csv-version "$VERSION" --default-channel --csv-channel=rhmi --update-crds --from-version "$PREVIOUS_VERSION"
+  operator-sdk generate csv --operator-name "integreatly-operator" --csv-version "$VERSION" --default-channel --csv-channel=rhmi --update-crds --from-version "$PREVIOUS_VERSION"
 }
 
 # Creating a new release version
